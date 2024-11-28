5cc4b4cd9a9659e7f62e6c3fc9799982d3e39e60
sam <samuelpinguin2007@gmail.com>
2024-11-28 13:20:31 -0500

    improved the code

--- functions.py
+++ functions.py
@@ -1,3 +1,10 @@
+#takes 2 numbers, x and y, and checks if the numbers between them, including x and excludin y
+#are divisible by 3, divisible by 5, or divisable by both.
+#if they dont meet those "requirements", it will just print out the number.
+
+start = int(input("what number will be the start? (Included): "))
+finish = int(input("what number will be the end? (Excluded): "))
+
 def function(x, y):
     for number in range (x,y):
         if number % 3 == 0 and number % 5 == 0:
@@ -9,4 +16,4 @@ def function(x, y):
         else:
             print(number)
     
-function(0, 5)
+function(start, finish)
