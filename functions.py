c7aaf8dd47987533062aa2f492dcb6b7734655f2
sam <samuelpinguin2007@gmail.com>
2024-11-28 13:12:37 -0500

    added the functions section

---
+++ functions.py
@@ -0,0 +1,12 @@
+def function(x, y):
+    for number in range (x,y):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+    
+function(0, 5)
