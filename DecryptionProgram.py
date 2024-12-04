a6961f7eb05a6409bd46ed9beac80e5f96dae6e8
SamCheesy <samuelpinguin2007@gmail.com>
2024-12-04 00:37:19 -0500

    added web directory for future web-related topics and practices

--- DecryptionProgram.py
+++
@@ -1,9 +0,0 @@
-import base64
-
-def decrypt(passw):
-    decrypted_bytes = base64.b64decode(passw)
-    decrypted_data = decrypted_bytes.decode()
-    print(decrypted_data)
-
-user_password = input("Password to decrypt: ")
-decrypt(user_password)
--- EncryptionProgram.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-def encryptPass(passw):
-    encodedBytes = base64.b64encode(passw.encode())
-    print(encodedBytes)
-
-user_pass = input("enter your password: ")
-encryptPass(user_pass)
